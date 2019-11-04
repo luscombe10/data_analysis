500d12cfb91e403b93f890081bcee6a2f3ebe10f
Henry Luscombe <hlusco01@dcs.bbk.ac.uk>
2019-11-04 22:26:52 +0000

    added some basic templates to pages

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from flask_bootstrap import Bootstrap
+
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route("/")
 def index():
+    user_agent = "Henry"
+    return render_template("main.html", name=user_agent)
+
+
+@app.route("/test")
+def test():
     return "<h1>Hello World</h1>"
+
+
+@app.route("/user")
+def user():
+    return render_template("user.html", name="Henry")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
